7f8d21871b41b62d8b3ced072b94d5aa7b27fa18
jlalovi <jlalovi@gmail.com>
2015-11-29 18:27:28 +0100

    v1.1

--- js/libreria.js
+++ js/libreria.js
@@ -27,7 +27,10 @@ function insertarFicha() {
 				alert("¡Ha ganado el jugador ROJO!");
 			else
 				alert("¡Ha ganado el jugador VERDE!");
-			location.reload();
+			var celdasTablero = getElementsByAttribute("cell");
+			for (var i=0; i<celdasTablero.length; i++) {
+				celdasTablero[i].className="celda";
+			}
 		}
 	};
 }
@@ -154,44 +157,14 @@ function partidaGanada(celda_ocupada) {
 	if (contador===4) return true;
 }
 
-/*
-// NOTA: El posicionamiento lo obtengo a través del método offsetTop, cuyo valor en la primera celda
-// de cada columna 'col[1]', es 26px. Este valor incrementa en 74px por cada celda, correspondiendo 
-// a la última celda el valor de 470px.
-function partidaGanada(celda_ocupada) {
-	var contador=1;
-	var valor = celda_ocupada.className;
-	var celda = celda_ocupada; // El valor de 'celda' cambiará en cada uno de los bucles a sus celdas hermanas.
-	
-	// VERTICALES:
-	while (celda.nextElementSibling!==null && celda.nextElementSibling.className==valor) {
-		contador++;
-		celda = celda.nextElementSibling;
-	}
-	celda = celda_ocupada; // reinicio la celda a su posición original
-	while (celda.previousElementSibling!==null && celda.previousElementSibling.className==valor) {
-		contador++;
-		celda = celda.previousElementSibling;
-	}
-	if (contador>=4) return true;
-
-	// Reiniciando contadores.
-	contador=1;
-	celda = celda_ocupada;
-
-	// HORIZONTALES:
-	var pos = celda_ocupada.offsetTop; // Posición de la celda ocupada (con respecto a la altura)
-	// derecha
-	while (celda.parentNode.nextElementSibling!==null && celda==valor)
-	for (var i=1; i<=7; i++) {
-		var col_sig = celda.parentNode.nextElementSibling;
-		if (col_sig.children[i].offsetT == pos) {
-
-		}
-	}
-	
-
-	return false;
+// Como no existe esta función, que creo útil, me la invento.
+function getElementsByAttribute(attribute) {
+  var coincidencias = [];
+  var elementos = document.getElementsByTagName('*'); // Todos los elementos de la página
+  for (var i = 0, n = elementos.length; i < n; i++) {
+    if (elementos[i].getAttribute(attribute) !== null) { // Compruebo uno a uno si los elementos tienen ese atributo
+      coincidencias.push(elementos[i]); // Si tiene ese atributo, lo guardo en el array de coincidencias.
+    }
+  }
+  return coincidencias;
 }
-
-*/
